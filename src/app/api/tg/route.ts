6180f8726638135ef9f3dce7f35fc16a866da1bd
SirMrAlierfan <alierfan.khanlari99@email.com>
2025-12-29 21:21:53 +0330

    first step

--- src/app/api/tg/route.ts
+++ src/app/api/tg/route.ts
@@ -1,11 +1,13 @@
-import { NextRequest, NextResponse } from "next/server";
-import "@/bot/modules/start/start.handler"
-export async function GET() {
-  return NextResponse.json({ ok: true });
-}
+import { NextRequest, NextResponse } from 'next/server';
+
+
+import '@/bot/modules/start/start.handler';
+import { bot } from '@/bot/modules/bot';
 
 export async function POST(req: NextRequest) {
-  const body = await req.json();
-  console.log("telegram update:", body);
+  const update = await req.json();
+
+  await bot.handleUpdate(update);
+
   return NextResponse.json({ ok: true });
 }
--- src/bot/modules/start/start.handler.ts
+++ src/bot/modules/start/start.handler.ts
@@ -5,26 +5,22 @@ import { bot } from "../bot";
 bot.start((ctx) => {
   ctx.reply(
     'یکی از گزینه‌ها را انتخاب کن:',
-    Markup.keyboard([
-      [
-        Markup.button.callback('magic', 'BTN_1'),
-        Markup.button.callback('test', 'BTN_2')
-      ],
-      [
-        Markup.button.callback('دکمه ۳', 'BTN_3')
-      ],
-      [
-         Markup.button.callback('magic', 'BTN_1'),
-        Markup.button.callback('test', 'BTN_2')
-      ],
-      [
-        Markup.button.callback('دکمه ۴', 'BTN_4')
-      ]
-    ])
+   Markup.inlineKeyboard([
+  [
+    Markup.button.callback('magic', 'BTN_1'),
+    Markup.button.callback('test', 'BTN_2')
+  ],
+  [
+    Markup.button.callback('دکمه ۳', 'BTN_3')
+  ],
+  [
+    Markup.button.callback('دکمه ۴', 'BTN_4')
+  ]
+])
+
   );
 });
 bot.action('BTN_1', (ctx) => ctx.reply('شما دکمه ۱ را فشار دادید'));
 bot.action('BTN_2', (ctx) => ctx.reply('شما دکمه ۲ را فشار دادید'));
 bot.action('BTN_3', (ctx) => ctx.reply('شما دکمه ۳ را فشار دادید'));
 bot.action('BTN_4', (ctx) => ctx.reply('شما دکمه ۴ را فشار دادید'));
-bot.launch();
