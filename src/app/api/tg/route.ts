bba2577d23eeba447b971c80c27ee4ab2162a342
SirMrAlierfan <alierfan.khanlari99@email.com>
2025-12-29 11:13:56 +0330

    still trying to conect webhook

--- src/app/api/tg/route.ts
+++ src/app/api/tg/route.ts
@@ -1,5 +1,11 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET() {
+  return NextResponse.json({ ok: true });
+}
+
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  console.log("telegram update:", body);
   return NextResponse.json({ ok: true });
 }
