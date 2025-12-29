b77cf5a6a02f681683e97a32b21c636fc5914130
SirMrAlierfan <alierfan.khanlari99@email.com>
2025-12-29 10:53:12 +0330

    webhook conection

--- src/app/api/tg/route.ts
+++ src/app/api/tg/route.ts
@@ -1,21 +1,5 @@
-import { NextRequest, NextResponse } from "next/server";
-import { Telegraf } from "telegraf";
+import { NextResponse } from "next/server";
 
-const bot = new Telegraf(process.env.BOT_TOKEN!);
-
-bot.start(ctx => ctx.reply("Bot is alive"));
-    
-export async function POST(req: NextRequest) {
-  try {
-    const update = await req.json();
-    await bot.handleUpdate(update);
-    return NextResponse.json({ ok: true });
-  } catch (err) {
-    console.error(err);
-    return NextResponse.json({ ok: false }, { status: 500 });
-  }
-}
-
-export function GET() {
-  return NextResponse.json({ status: "OK" });
+export async function GET() {
+  return NextResponse.json({ ok: true });
 }
