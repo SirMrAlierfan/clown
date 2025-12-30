7cd3cacc046c784345aa1ffe4434d706b7ad6851
SirMrAlierfan <alierfan.khanlari99@email.com>
2025-12-30 21:12:31 +0330

    add commends to robot

--- src/bot/modules/groupManger/manger.handler.ts
+++ src/bot/modules/groupManger/manger.handler.ts
@@ -36,13 +36,13 @@ bot.on("text", async (ctx) => {
       break;
 
     case "unban":
-    case "آزاد":
+    case "حذف بن":
       await bot.telegram.unbanChatMember(chatId, originalUserId);
       await ctx.reply(`کاربر @${originalUsername} آزاد شد.`);
       break;
 
     case "unsilent":
-    case "آن‌سایلنت":
+    case "حذف سکوت":
       await bot.telegram.restrictChatMember(chatId, originalUserId, {
         permissions: {
           can_send_messages: true,
@@ -54,6 +54,53 @@ bot.on("text", async (ctx) => {
       await ctx.reply(`کاربر @${originalUsername} می‌تواند دوباره پیام ارسال کند.`);
       break;
 
+    case "promote":
+    case "ترفیغ":
+      await bot.telegram.promoteChatMember(chatId, originalUserId, {
+        can_change_info: true,
+        can_delete_messages: true,
+        can_edit_messages: true,
+        can_manage_chat: true,
+        can_invite_users: true,
+        can_manage_topics: true,
+        can_pin_messages: true,
+        can_manage_video_chats: true,
+        can_post_messages: true,
+        can_restrict_members: true
+      })
+       ctx.reply(`کابر ${originalUsername}مدیر است`)
+      break
+    case "unpromote":
+    case "عزل":
+      await bot.telegram.promoteChatMember(chatId, originalUserId, {
+        can_change_info: false,
+        can_delete_messages: false,
+        can_edit_messages: false,
+        can_manage_chat: false,
+        can_invite_users: false,
+        can_manage_topics: false,
+        can_pin_messages: false,
+        can_manage_video_chats: false,
+        can_post_messages: false,
+        can_restrict_members: false
+      })
+       ctx.reply(`کابر ${originalUsername}مدیر نیست`)
+      break
+    case "pin":
+    case "پین":
+      await bot.telegram.pinChatMessage(chatId, ctx.message.reply_to_message.message_id)
+       ctx.reply(`پیام پین شد`)
+      break
+    case "unpin":
+    case "حذف پین":
+      await bot.telegram.pinChatMessage(chatId, ctx.message.reply_to_message.message_id)
+       ctx.reply(`پیام از پین درآمد`)
+      break
+    case "del":
+    case "حذف":
+      await bot.telegram.deleteMessage(chatId, ctx.message.reply_to_message.message_id)
+       ctx.reply(`پیام حذف شد`)
+      break
     default:
       break;
   }
