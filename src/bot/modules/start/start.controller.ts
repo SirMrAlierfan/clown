cb58ee980554fcb44f9e11304b71249d3888e942
SirMrAlierfan <alierfan.khanlari99@email.com>
2025-12-30 10:35:15 +0330

    add privet msg

--- src/bot/modules/start/start.controller.ts
+++ src/bot/modules/start/start.controller.ts
@@ -1,9 +1,54 @@
 import { bot } from "../bot";
 
-bot.action("gap",(ctx)=>{
-    ctx.reply("https://t.me/joinchat/qc6GaSXlxktmMDA0")
+const userMessages: Record<number, string> = {};
+let userId: number | undefined
+let userName: string
+bot.action("gap", (ctx) => {
+    ctx.reply("https://t.me/joinchat/qc6GaSXlxktmMDA0");
+});
+bot.action("Idfounder", async (ctx) => {
+    userId = ctx.chat?.id
+    userName = ctx.from.first_name
+    await ctx.reply(`your id is : ${userId}\n for:${userName}`)
 })
-bot.action("pvMsg",(ctx)=>{
-    ctx.answerCbQuery()
-    ctx.reply("write your msg")
-})
+bot.action("pvMsg", async (ctx) => {
+    await ctx.answerCbQuery();
+    await ctx.reply("write your msg");
+
+    const handler = (ctx2: any) => {
+        const userId = ctx2.from.id;
+
+        if (!userMessages[userId]) {
+            const msg = ctx2.message.text;
+            userMessages[userId] = msg;
+
+            ctx2.reply("your message has been saved!");
+
+            bot.telegram.sendMessage(7584261287, `Message from @${ctx2.from.username || ctx2.from.first_name}:\n${msg}`);
+
+
+        }
+    };
+
+    bot.on('text', handler);
+});
+bot.action("privetMsg", async (ctx) => {
+    await ctx.answerCbQuery();
+    await ctx.reply("write your msg to batman as privet");
+
+    const handler = (ctx2: any) => {
+
+        const msg = ctx2.message.text;
+
+
+        ctx2.reply("your message has been delverd!");
+
+        bot.telegram.sendMessage(7584261287, `Message :${msg}`);
+
+
+
+
+    };
+
+    bot.on('text', handler);
+});
--- src/bot/modules/start/start.handler.ts
+++ src/bot/modules/start/start.handler.ts
@@ -1,43 +1,23 @@
 import { Markup } from 'telegraf';
 import { bot } from '@/bot/modules/bot';
 
-const adminId = 123456789; // آیدی خودت در تلگرام
-const userMessages: Record<number, string> = {}; // ذخیره پیام‌ها بر اساس user id
+
 
 bot.start(async (ctx) => {
   await ctx.reply(
     'what do you want from this clown',
     Markup.inlineKeyboard([
       [
-        Markup.button.callback('circus addres', 'gap'),
-        Markup.button.callback('pv to batman', 'pvMsg')
+        Markup.button.callback('circus addres', 'gap')
+      ],
+      [
+        Markup.button.callback('pv to batman', 'pvMsg')]
+      ,
+      [
+        Markup.button.callback("what is my id?", "IdFounder")
+      ], [
+        Markup.button.callback("send privet Msg", "privetMsg")
       ]
     ])
   );
-});
-
-bot.action("gap", (ctx) => {
-  ctx.reply("https://t.me/joinchat/qc6GaSXlxktmMDA0");
-});
-
-bot.action("pvMsg", async (ctx) => {
-  await ctx.answerCbQuery();
-  await ctx.reply("write your msg");
-
-  const handler = (ctx2: any) => {
-    const userId = ctx2.from.id;
-
-    if (!userMessages[userId]) {
-      const msg = ctx2.message.text;
-      userMessages[userId] = msg;
-
-      ctx2.reply("your message has been saved!");
-
-      bot.telegram.sendMessage(7584261287, `Message from @${ctx2.from.username || ctx2.from.first_name}:\n${msg}`);
-
-      
-    }
-  };
-
-  bot.on('text', handler);
 });
