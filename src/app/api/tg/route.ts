b461365c357b3bdeb9b02fb1156066e8bd8e0d73
SirMrAlierfan <alierfan.khanlari99@email.com>
2025-12-30 17:47:58 +0330

    fix erorrs

--- src/app/api/tg/route.ts
+++ src/app/api/tg/route.ts
@@ -1,10 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
-
+import { bot } from "@/bot/modules/bot";
 import "@/bot/modules/start/start.handler"; // register handlers
 import "@/bot/modules/start/start.controller"
 import "@/bot/modules/groupManger/manger.controller"
 import "@/bot/modules/groupManger/manger.handler"
-import { bot } from "@/bot/modules/bot";
+
 
 export async function POST(req: NextRequest) {
   const update = await req.json();
