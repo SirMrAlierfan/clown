f675868e3217e677dcd68dbbea446fccfc449bb8
SirMrAlierfan <alierfan.khanlari99@email.com>
2025-12-30 18:13:34 +0330

    fix: robot err for privcy chenge to commend form

--- src/bot/modules/groupManger/manger.handler.ts
+++ src/bot/modules/groupManger/manger.handler.ts
@@ -1,22 +1,28 @@
 import { bot } from '@/bot/modules/bot';
 
 
-bot.hears("hi", (ctx) => ctx.reply("hi"));
 
-bot.on("text", async (ctx) => {
-  const messageText = ctx.message.text.toLowerCase();
-  const chatId = ctx.chat.id;
-  const userId = ctx.from.id;
+const adminCommands = ["ban", "silent", "unban", "unsilent"];
 
-  const adminList = await ctx.getChatAdministrators();
-  const isAdmin = adminList.some(admin => admin.user.id === userId);
-  if (!isAdmin) return;
+adminCommands.forEach((cmd) => {
+  bot.command(cmd, async (ctx) => {
+    const chatId = ctx.chat.id;
+    const userId = ctx.from.id;
 
-  if (ctx.message.reply_to_message?.from?.id) {
+  
+    const adminList = await ctx.getChatAdministrators();
+    const isAdmin = adminList.some(admin => admin.user.id === userId);
+    if (!isAdmin) return ctx.reply("only Admins!!!!");
+
+   
+    if (!ctx.message.reply_to_message?.from?.id) {
+      return ctx.reply("reply on someone");
+    }
+
     const originalUserId = ctx.message.reply_to_message.from.id;
     const originalUsername = ctx.message.reply_to_message.from.username || ctx.message.reply_to_message.from.first_name;
 
-    switch (messageText) {
+    switch (cmd) {
       case "ban":
         await bot.telegram.banChatMember(chatId, originalUserId);
         await ctx.reply(`User @${originalUsername} got banned`);
@@ -26,7 +32,6 @@ bot.on("text", async (ctx) => {
         await bot.telegram.restrictChatMember(chatId, originalUserId, {
           permissions: {
             can_send_messages: false,
-
             can_send_polls: false,
             can_send_other_messages: false,
             can_add_web_page_previews: false
@@ -44,7 +49,6 @@ bot.on("text", async (ctx) => {
         await bot.telegram.restrictChatMember(chatId, originalUserId, {
           permissions: {
             can_send_messages: true,
-
             can_send_polls: true,
             can_send_other_messages: true,
             can_add_web_page_previews: true
@@ -52,9 +56,6 @@ bot.on("text", async (ctx) => {
         });
         await ctx.reply(`User @${originalUsername} can now send messages`);
         break;
-
-      default:
-        break;
     }
-  }
+  });
 });
